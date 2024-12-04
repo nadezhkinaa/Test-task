ada8c2782d8a876b1623a7038300981f4b272a6f
ann.nadezhkina <ann.nadezhkina@mail.ru>
2024-12-04 21:31:05 +0300

    add handler button and function change value of priority

--- test/script.js
+++ test/script.js
@@ -1,50 +1,52 @@
 $(document).ready(function() {
     // fetching data from json file
     $.getJSON("table.json", function(data) {
-        console.log(data);
-
         let temp = "";
-
-        // iterating through objects
         $.each(data, function(key, value) {
             temp += "<tr>";
-
             temp += "<td>" + value.name + "</td>";
-
             temp += "<td>" + value.position + "</td>";
-
             temp += "<td class='priority-cell'>" + value.priority + "</td>";
-
             temp += "<td>" + value.max_priority + "</td>";
-
             temp += "</tr>";
         });
-
         $("#table").append(temp);
-
-        sortTable(2, 1); // 2 - индекс столбца "Приоритет" (нумерация с 0)
+        sortTable(2, 1);
     });
-    //handler for clicking on a cell
+
+    //handler for clicking on a row
     $("#table").on("click", "tr", function(event) {
         if (event.ctrlKey) {
-            // Ctrl + click:
             $(this).toggleClass("active");
         } else {
-            // Single click:
             $("#table tr").removeClass("active");
             $(this).addClass("active");
         }
-
-        const rowIndex = $(this).index() - 1;
-
-        const priorityCell = $(this).find(".priority-cell");
-        const priority = parseInt(priorityCell.text(), 10);
-
-        console.log("Row Index:", rowIndex);
-        console.log("Priority:", priority);
     });
 
-    // sorting
+    // Save button click handler
+    $(".save-button").click(function() {
+        const selectedRow = $("#table tr.active");
+        if (selectedRow.length === 0) {
+            alert("Выберите строку!");
+            return;
+        }
+
+        const newPriority = parseInt($(".priority-select").val(), 10);
+        const maxPriority = parseInt(selectedRow.find("td:last-child").text(), 10);
+
+        if (isNaN(newPriority) || newPriority > maxPriority || newPriority < 0) {
+            alert(
+                "Некорректный приоритет. Приоритет должен быть числом от 0 до максимального приоритета."
+            );
+            return;
+        }
+
+        selectedRow.find(".priority-cell").text(newPriority);
+        sortTable(2, 1);
+    });
+
+    // sorting function
     function sortTable(colIndexPriority, colIndexPosition) {
         const rows = $("#table tr:gt(0)");
         rows.sort(function(a, b) {
@@ -73,8 +75,6 @@ $(document).ready(function() {
                 return aPosition.localeCompare(bPosition);
             }
         });
-        $.each(rows, function(index, row) {
-            $("#table").append(row);
-        });
+        $("#table").append(rows);
     }
 });
